e8d5ca79403fc23a3d7da10a6f94f391d7fd0ab3
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-10-15 05:20:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,6 @@
 #write your code here
 def countdown(number)
-  while number == 0
+  while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1 
   end
@@ -8,11 +8,10 @@ def countdown(number)
 end
 
 def countdown_with_sleep(number)
-  while number == 0
+  while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1 
+    sleep 1 
   end
   "HAPPY NEW YEAR!"
 end
-
-sleep 1
